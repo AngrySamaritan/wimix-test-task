1e6466df4734dba7b39969d4be88c749464371ab
Andrei Trukhan <andrey-truhan@tut.by>
2020-08-31 20:49:20 +0300

    Init

---
+++ src/main/java/com/angrysamaritan/wimixtest/WimixTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.angrysamaritan.wimixtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WimixTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WimixTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/angrysamaritan/wimixtest/WimixTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angrysamaritan.wimixtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WimixTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
