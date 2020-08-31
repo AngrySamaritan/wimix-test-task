89906e7abe0b50e5f41b9b7329646e558b1f962c
Andrei Trukhan <andrey-truhan@tut.by>
2020-08-31 21:03:29 +0300

    User model

---
+++ src/main/java/com/angrysamaritan/wimixtest/model/User.java
@@ -0,0 +1,40 @@
+package com.angrysamaritan.wimixtest.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    private int id;
+
+    @Getter
+    @Setter
+    private String username;
+
+    @Getter
+    @Setter
+    private String password;
+
+    @Getter
+    @Setter
+    private String firstName;
+
+    @Getter
+    @Setter
+    private String lastName;
+
+
+    @Getter
+    @Setter
+    private String email;
+
+}
